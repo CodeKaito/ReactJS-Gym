5f3dddee7a960ca4e174f7a1754ec014e91626c1
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-07 19:18:44 +0200

    update

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Stack, Typography } from '@mui/material'
 
-const BodyPart = () => {
+import Icon from '../assets/icons/gym.png'
+
+const BodyPart = ({ item, setBordyPart, bodyPart }) => {
   return (
-    <div>BodyPart</div>
+    <Stack 
+      type='button' alignItems='center' justifyContent='center' className='bodyPart-card'
+      sx={{ borderTop: bodyPart === item ? '4px solid #ff2625' : '', 
+      backgroundColor: '#fff', borderBottomLeftRadius: '20px', width: '270px', height:'280px', cursor: 'pointer', gap: '47px' }} >
+        
+      <img src={Icon} alt="dumbbell" styles={{ width: '40px', height:'40px' }}/>
+    </Stack>
   )
 }
 
--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -2,15 +2,17 @@ import { Box } from '@mui/material'
 import React from 'react'
 import BodyPart from './BodyPart'
 
+import { ScrollMenu } from 'react-horizontal-scrolling-menu'
+
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <div>
+    <ScrollMenu>
         {data.map((item) => 
         <Box key={item.id || item} itemId={item.id || item} title={item.id || item} margin='0 40px'>
             <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
         )}
-    </div>
+    </ScrollMenu>
   )
 }
 
