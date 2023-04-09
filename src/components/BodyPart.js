ad0da81546f6348a187eee78457b171bb872c46c
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-09 19:02:29 +0200

    Update BodyPart.js

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,14 +3,18 @@ import { Stack, Typography } from '@mui/material'
 
 import Icon from '../assets/icons/gym.png'
 
-const BodyPart = ({ item, setBordyPart, bodyPart }) => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
     <Stack 
       type='button' alignItems='center' justifyContent='center' className='bodyPart-card'
       sx={{ borderTop: bodyPart === item ? '4px solid #ff2625' : '', 
-      backgroundColor: '#fff', borderBottomLeftRadius: '20px', width: '270px', height:'280px', cursor: 'pointer', gap: '47px' }} >
-        
-      <img src={Icon} alt="dumbbell" styles={{ width: '40px', height:'40px' }}/>
+      backgroundColor: '#fff', borderBottomLeftRadius: '20px', width: '270px', height:'280px', cursor: 'pointer', gap: '47px' }} 
+      onClick={() => { setBodyPart(item); }} >
+
+      <img src={Icon} alt="dumbbell" style={{ width: '40px', height:'40px' }}/>
+      <Typography fontSize='24px' fontWeight='bold' color='3A1212' textTransform='capitalize'>
+        {item}
+      </Typography>
     </Stack>
   )
 }
